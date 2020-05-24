9d5a3725f531ff4baaf1267f9487aa294f33b64c
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-05-24 13:06:54 -0400

    Add tuple_num_replace to write_pages method

--- main.py
+++ main.py
@@ -4,9 +4,4 @@ import sys, time, random, re
 story = {}
 
 en.write_page(story)
-en.replace_tuple_num(story)
-counter = 1
-page = story[counter]['Text']
-for line in page:
-    en.slow_type(line)
-#en.get_response(story[1]['Options'])
+en.story_flow(story)
