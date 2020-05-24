d225ab149d61e11fb956c42d15a234db2fb91894
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-05-24 12:45:25 -0400

    Modify write_page, loop_pages methods
    The write_page method tests that a line from the story file meets a certain criteria (is or is not a Page or Options header) and passes line into loop_pages method. The loop_pages method adds the line to the Text array if the line is not an Options header, a Page header, or an option. If it is an Options header, it prints an alert to the console instead of inserting the line into the Text array. If the line is an option, it writes the option text and the page number into a tuple in the Options array.

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-import sys, time, random
+import sys, time, random, re
 
 story = {
   1: {'Text': [ "Hello there..", "I bet you werent exepecting to hear from me so soon...", "...you seem a little confused do you know who I am?"],
@@ -10,38 +10,84 @@ def basic_dict(target_dict, page_num):
     #print(target_hash)
     return target_dict
 
+f = open("story.txt", "r")
+
+#Do text, options, and page appending separately
+
+def loop_pages(user_input, input_dict, page_count):
+
+    if user_input != 'Options\n' and user_input != 'Page {}\n'.format(page_count) and user_input != '\n' and re.search("([A-z]+\W*,\s*\d)", user_input) == None:
+        input_dict[page_count]['Text'].append(user_input)
+        #print(input_dict[page_count]['Text'])
+
+    if user_input == 'Options':
+        print("Found an options header")
+    elif user_input != 'Options' and re.search("([A-z]+\W*,\s*\d)", user_input) != None:
+        input_dict[page_count]['Options'].append((tuple(user_input.replace('\n', '').split(","))))
+        """#while user_input != 'Page {}'.format(page_count + 1):
+            #if user_input == 'Page {}'.format(page_count + 1):
+                #break
+
+        option_text = user_input
+        try:
+            option_num = int(user_input)
+        except:
+            return False
+
+        input_dict[page_count]['Options'].append((user_input, option_num))
+        print(input_dict[page_count]['Options'])
+
+    elif user_input == 'Page {}'.format(page_count + 1):
+        print('Hello from page {}'.format(page_count + 1))
+        return True"""
+    """while user_input != 'o' and user_input != 'quit' and user_input != 'Next':
+        input_dict[page_count]['Text'].append(user_input)
+        print(input_dict[page_count]['Text'])
+        user_input = input('')
+
+    if user_input == 'o':
+        #user_input = input('Moving to options settings...')
+        while user_input != 'quit' and user_input != 'Next':
+
+            if user_input == 'Next':
+                break
+
+            user_input = input("Enter the text of the option: ")
+
+            try:
+                option_num = int(input("Enter the number of the page to jump to: "))
+            except:
+                #print(user_input)
+                break
+
+            input_dict[page_count]['Options'].append((user_input, option_num))
+            print(input_dict[page_count]['Options'])
+
+    if user_input == 'quit' or user_input == 'Next':
+            print("hello")"""
+
+
 test = {}
 
 def write_page():
-    print("Starting on page 1. Enter input line-by-line")
-
     page_counter = 1
     basic_dict(test, page_counter)
-    line = input('')
 
-    if line == 'Next page':
-        page_counter += 1
-        #loop_pages(page_counter)
-
-def loop_pages(user_input, dictionary, page_count):
-    while user_input != 'Options' and user_input != 'quit' and user_input != 'Next page':
-        test[page_counter]['Text'].append(user_input)
-        print(test[page_counter]['Text'])
-        user_input = input('')
-
-    if user_input == 'Options':
-        while user_input != 'quit' and user_input != 'Next page':
-            option_text = input("Enter the text of the option: ")
-            option_num = int(input("Enter the number of the page to jump to: "))
-
-            test[page_counter]['Options'].append((option_text, option_num))
-            print(test[page_counter]['Options'])
-
-            if user_input == 'quit' or user_input == 'Next page':
-                break
-
-
+    for line in f:
+        if line == 'Page {}\n'.format(page_counter + 1):
+            page_counter += 1
+            basic_dict(test, page_counter)
+            loop_pages(line, test, page_counter)
+        elif line == 'Options':
+            loop_pages(line, test, page_counter)
+        elif line == '\n':
+            line.replace("\n", "")
+        elif line != 'Options' and re.search("([A-z]+\W*,\s*\d)", line) != None:
+            loop_pages(line, test, page_counter)
+        else:
+            loop_pages(line, test, page_counter)
 write_page()
+print(test)
 
 def slow_type(string):
     type_speed = 50
